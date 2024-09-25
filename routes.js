34a54696e940bde3cfffaf7e6725e9934df7d11d
GanymedeWordsmyth <128321181+GanymedeWordsmyth@users.noreply.github.com>
2024-09-24 21:06:18 -0400

    Dynamic css

--- routes.js
+++ routes.js
@@ -2,6 +2,10 @@ const routes = [
     { page: 'Home', path: '/' },
     { page: 'About', path: '/assets/pages/about.html'},
     { page: 'Tech', path: '/assets/pages/tech.html'},
+    { css: { 
+        main: "/assets/css/styles.css",
+        theme: "/assets/css/theme.css"
+    },}
 ];
 
 export default routes;
